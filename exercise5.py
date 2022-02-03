17a5c61b1c71f801b9f4a9239d691a7b47bfb5fb
alvhndz <93944011+alvhndz@users.noreply.github.com>
2022-02-03 17:30:39 -0600

    Add files via upload

---
+++ exercise5.py
@@ -0,0 +1,17 @@
+from lcg import prng
+
+sample_size = 100000
+bins = 10
+
+# let us split the sampled random numbers into equal bins
+sample = [prng()//(2**64//bins) for _ in range(sample_size)]
+
+def get_frequencies():
+    """return the list of frequencies for successive bins"""
+    frequencies = dict()
+    for bin_ in sample:
+        frequencies[bin_] = frequencies.get(bin_, 0) + 1
+    return list(frequencies.values())
+
+print(*get_frequencies())
+
