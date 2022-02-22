315ceff871fc3f824a9d0ec699ed4852366abfa8
alvhndz <93944011+alvhndz@users.noreply.github.com>
2022-02-22 14:02:25 -0600

    Update exercise_8.py

--- exercise_8.py
+++ exercise_8.py
@@ -1,25 +1,30 @@
 #exercise_8.py
+import math
 
-h = a;
-h_prime = m;
-p = 1;
-p_prime = 0;
-s = 1 + a**2;
+a= 123456789
+m=2**64
+h = a
+h_prime = m
+p = 1
+p_prime = 0
+s = 1 + a**2
 
-q = [h_prime / h];
-u = h_prime - q*h;
-v = p_prime - q*p;
+q = h_prime / h
+u = h_prime - (q*h)
+v = p_prime - q*p
 
 if u**2 + v**2 < s:
-s = u**2 + v**2
-h_prime = h;
-h = u;
-p_prime = p;
-p = v;
-
-u = u - h;
-v = v - p;
-
-if s = u**2 + v**2:
-v_2 = sqrt(s);
-
+	h_prime = h
+	h = u
+	p_prime = p
+	p= v
+	q = [h_prime / h]
+	u = h_prime - (q*h)
+	v = p_prime - q*p
+	s = u**2 + v**2
+else:
+	u = u - h
+	v = v - p
+	s = u**2 + v**2
+	v_2 = math.sqrt(s)
+	print(v_2)
